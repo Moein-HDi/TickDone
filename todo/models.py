793d8eb32a578dc41d21e65c4fac0550d9478e94
Moein-HDi <moein.hedayati@gmail.com>
2023-04-12 20:07:03 +0330

    initiating new project

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TodoList(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class TodoItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    todo_list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
